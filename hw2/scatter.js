5bcbd3bb4197e86bd0dece36328ea18ef6a65869
Amenova Shahnaz <shakhnazamenova@gmail.com>
2022-03-01 00:26:05 +0600

    done

--- hw2/scatter.js
+++ hw2/scatter.js
@@ -1,49 +1,105 @@
-async function scatterPlot() {
-  console.log("scatterPlot")
-  const data = await d3.json("forviz.json");
-  console.log(data)
-  const xAcc = d => d[0];
-  const yAcc = d => d[1];
-  let dimensions = {
-    width: window.innerWidth*0.5,
-    height: 300,
-    margin: {
-      top: 30,
-      left: 30,
-      bottom: 30,
-      right: 30
-    }
-  }
+async function drawScatter() {
+    // 1. Access data
+    let dataset = await d3.json("my_weather_data.json");
 
-  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right;
-  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+    const xAccessor = (d) => d.dewPoint;
+    const yAccessor = (d) => d.humidity;
+    const colorAccessor = (d) => d.cloudCover;
 
-  let wrapper = d3.select("#wrapper").append("svg");
-  wrapper.attr("width", dimensions.width);
-  wrapper.attr("height", dimensions.height);
-  let container = wrapper.append("g");
-  container.attr("transform",`translate(${dimensions.margin.left},${dimensions.margin.top})`);
-  let xScale = d3.scaleLinear()
-    .domain(d3.extent(data,xAcc))
-    .range([dimensions.margin.left, dimensions.boundedWidth]);
+    // 2. Create chart dimensions
 
-  let yScale = d3.scaleLinear()
-    .domain(d3.extent(data,yAcc))
-    .range([dimensions.margin.top, dimensions.boundedHeight]);
-  let viz = container.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", d=>xScale(xAcc(d)))
-    .attr("cy", d=>yScale(yAcc(d)))
-    .attr("r","5px")
-    .attr("fill","#AA1111");
-  let xAxisGen = d3.axisBottom().scale(xScale);
-  let yAxisGen = d3.axisLeft().scale(yScale);
-  const axisX = container.append("g").call(xAxisGen).style("transform",`translateY(${dimensions.boundedHeight}px)`)
-  const axisy = container.append("g").call(yAxisGen).style("transform",`translateX(${dimensions.margin.left}px)`)
+    const width = d3.min([window.innerWidth * 0.9, window.innerHeight * 0.9]);
+    let dimensions = {
+        width: width,
+        height: width,
+        margin: {
+            top: 10,
+            right: 10,
+            bottom: 50,
+            left: 50,
+        },
+    };
+    dimensions.boundedWidth =
+        dimensions.width - dimensions.margin.left - dimensions.margin.right;
+    dimensions.boundedHeight =
+        dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
+    // 3. Draw canvas
 
+    const wrapper = d3
+        .select("#wrapper")
+        .append("svg")
+        .attr("width", dimensions.width)
+        .attr("height", dimensions.height);
+
+    const bounds = wrapper
+        .append("g")
+        .style(
+            "transform",
+            `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`,
+        );
+
+    // 4. Create scales
+
+    const xScale = d3
+        .scaleLinear()
+        .domain(d3.extent(dataset, xAccessor))
+        .range([0, dimensions.boundedWidth])
+        .nice();
+
+    const yScale = d3
+        .scaleLinear()
+        .domain(d3.extent(dataset, yAccessor))
+        .range([dimensions.boundedHeight, 0])
+        .nice();
+
+    const colorScale = d3
+        .scaleLinear()
+        .domain(d3.extent(dataset, colorAccessor))
+        .range(["skyblue", "darkslategrey"]);
+
+    // 5. Draw data
+
+    const dots = bounds
+        .selectAll("circle")
+        .data(dataset)
+        .enter()
+        .append("circle")
+        .attr("cx", (d) => xScale(xAccessor(d)))
+        .attr("cy", (d) => yScale(yAccessor(d)))
+        .attr("r", 4)
+        .attr("fill", (d) => colorScale(colorAccessor(d)))
+        .attr("tabindex", "0");
+
+    // 6. Draw peripherals
+
+    const xAxisGenerator = d3.axisBottom().scale(xScale);
+
+    const xAxis = bounds
+        .append("g")
+        .call(xAxisGenerator)
+        .style("transform", `translateY(${dimensions.boundedHeight}px)`);
+
+    const xAxisLabel = xAxis
+        .append("text")
+        .attr("x", dimensions.boundedWidth / 2)
+        .attr("y", dimensions.margin.bottom - 10)
+        .attr("fill", "black")
+        .style("font-size", "1.4em")
+        .html("Dew point (&deg;F)");
+
+    const yAxisGenerator = d3.axisLeft().scale(yScale).ticks(4);
+
+    const yAxis = bounds.append("g").call(yAxisGenerator);
+
+    const yAxisLabel = yAxis
+        .append("text")
+        .attr("x", -dimensions.boundedHeight / 2)
+        .attr("y", -dimensions.margin.left + 10)
+        .attr("fill", "black")
+        .style("font-size", "1.4em")
+        .text("Relative humidity")
+        .style("transform", "rotate(-90deg)")
+        .style("text-anchor", "middle");
 }
-
-scatterPlot()
+drawScatter();
